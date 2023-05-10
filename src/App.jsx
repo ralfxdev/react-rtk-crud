f06ca2dc2d4cbdb86bbb7a02ed8b1f8755076103
ralfxdev <lopezalexander1019@gmail.com>
2023-05-09 20:03:24 -0600

    change ext jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from 'react-redux'
-import './App.css'
+import { useSelector } from "react-redux";
+import "./App.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
-
-  const tasksState = useSelector(state => state.tasks)
-  console.log(tasksState)
+  const tasksState = useSelector((state) => state.tasks);
+  console.log(tasksState);
 
   return (
     <>
       <h1>Hello World!</h1>
+      <TaskForm />
+      <TaskList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
